32336f7720e53335ebe8a31209fbdf1fb68797e6
Ava Evans <avaevansm@gmail.com>
2016-06-22 16:20:36 -0400

    defined wordbank array, told ruby to put _ in place of word bank words ie letters

--- hangman.rb
+++ hangman.rb
@@ -5,6 +5,30 @@ until user_input_ok == "ok"
 end
 
 
-word_bank = [duck,book,cat,lamp,taco,kiss]
-word_bank.Set 
-puts generate_word
+turn = 0
+rematch = nil
+word_bank = ["duck","book","cat","taco"]
+if rematch == 'new' || turn == 0
+  word_bank.each do |x|
+    print  " _ "
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+#word_bank = [duck,book,cat,lamp,taco,kiss]
+#word_bank.Set
+
+
+#def to_array
+#  x = [duck,book,cat].to_a
+#  print x
+end
